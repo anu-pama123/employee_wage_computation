6ce620217492843c736193863129eb19c782c5c5
anupama cv <anu.anupamacv@gmail.com>
2021-05-23 23:18:00 +0530

    ADDED: functionality to calculate daily employee wage

--- src/main/java/com/bridgelabz/EmployeeWageBuilder.java
+++ src/main/java/com/bridgelabz/EmployeeWageBuilder.java
@@ -3,12 +3,18 @@ package com.bridgelabz;
 public class EmployeeWageBuilder
 {
     public static double Employee_check;
-    public static int IS_PRESENT=1;
-    public static void random_generator(int var)// method
+    public static final int IS_PRESENT=1;
+    public static final int WAGE_PER_HOUR=20;
+    public static final int FULL_DAY_HOUR=8;
+    public static double Daily_Employee_Wage;
+    public static void random_generator(int var)// method 1
     {
         Employee_check = Math.floor((Math.random() * 10)) % var;
     }
-
+    public static void employee_wage()// method 2
+    {
+        Daily_Employee_Wage=(WAGE_PER_HOUR * FULL_DAY_HOUR);
+    }
     public static void main(String[] args)
     {
         EmployeeWageBuilder object = new EmployeeWageBuilder();
@@ -16,7 +22,10 @@ public class EmployeeWageBuilder
         if (object.Employee_check == object.IS_PRESENT)
         {
             System.out.println("Employee is present");
-        } else
+            object.employee_wage();
+            System.out.println("Daily employee wage is = " + object.Daily_Employee_Wage);
+        }
+        else
         {
             System.out.println("Employee is absent");
         }
