7ac3a167e92db139dc0a0518adff035f87d90c78
anupama cv <anu.anupamacv@gmail.com>
2021-05-31 15:59:24 +0530

    REFACTORED: by including array list

--- src/main/java/com/bridgelabz/EmployeeWageBuilder.java
+++ src/main/java/com/bridgelabz/EmployeeWageBuilder.java
@@ -1,15 +1,18 @@
 package com.bridgelabz;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class EmployeeWageBuilder
 {
-    public static CompanyEmpWage objectArray[]=new CompanyEmpWage[2];
+    public static List<CompanyEmpWage> companyEmpWages = new ArrayList<CompanyEmpWage>();
     public static void main(String[] args)
     {
-        objectArray[0] = new CompanyEmpWage(20, 100, 20);
-        int employeeWageInfosys=objectArray[0].randomGenerator();//method calling
+        companyEmpWages.add(new CompanyEmpWage(20, 100, 20));
+        int employeeWageInfosys=companyEmpWages.get(0).randomGenerator();//method calling
         System.out.println("Monthly employee wage at Infosys = " + employeeWageInfosys);
-        objectArray[1] = new CompanyEmpWage(30, 110, 10);
-        int employeeWageTcs=objectArray[1].randomGenerator();//method calling
+        companyEmpWages.add(new CompanyEmpWage(30, 110, 10));
+        int employeeWageTcs=companyEmpWages.get(1).randomGenerator();//method calling
         System.out.println("Monthly employee wage at Tcs = " + employeeWageTcs);
     }
 }
