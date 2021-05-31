ddd4234404420ef2025d12aa241ebf6d3bdb0c14
anupama cv <anu.anupamacv@gmail.com>
2021-05-31 11:56:28 +0530

    REFACTORED: by including array of many company employee wage

--- src/main/java/com/bridgelabz/EmployeeWageBuilder.java
+++ src/main/java/com/bridgelabz/EmployeeWageBuilder.java
@@ -1,43 +1,17 @@
 package com.bridgelabz;
+
 public class EmployeeWageBuilder
 {
-    public  static final int isFullTime = 1;
-    public  static final int isPartTime = 2;
-    private  static double employeeWage;
-    private  static double employeeCheck;
-    public static int randomGenerator(int wagePerHr, int maxHr, int maxDay) // method
+    public static CompanyEmpWage objectArray[]=new CompanyEmpWage[2];
+    public static void main(String[] args)
     {
-        int empHR = 0;
-        int empWage = 0;
-        int totalWorkingHr=0;
-        int day=0;
-        while (totalWorkingHr < maxHr && day < maxDay)
-        {
-            employeeCheck = Math.floor((Math.random() * 10)) % 3;
-            switch ((int) employeeCheck)
-            {
-                case isFullTime:
-                    empHR = 8;
-                    break;
-                case isPartTime:
-                    empHR = 4;
-                    break;
-                default:
-                    empHR = 0;
-                    break;
-            }
-            empWage = empWage + (wagePerHr * empHR);
-            System.out.println("Daily employee wage = " + empWage);
-            day++;
-            totalWorkingHr = totalWorkingHr + empHR;
-        }
-        return empWage;
-
+        objectArray[0] = new CompanyEmpWage(20, 100, 20);
+        int employeeWageInfosys=objectArray[0].randomGenerator();//method calling
+        System.out.println("Monthly employee wage at Infosys = " + employeeWageInfosys);
+        objectArray[1] = new CompanyEmpWage(30, 110, 10);
+        int employeeWageTcs=objectArray[1].randomGenerator();//method calling
+        System.out.println("Monthly employee wage at Tcs = " + employeeWageTcs);
     }
-        public static void main(String[] args)
-        {
-            employeeWage=EmployeeWageBuilder.randomGenerator(20,100,20);//method calling
-            System.out.println("Monthly employee wage = " + employeeWage);
-        }
-
 }
+
+
